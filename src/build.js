3c3997c320bc6991ebd48535931c68e888a11152
maladr0it <bradford.lachlan@gmail.com>
2019-10-22 23:09:12 +0900

    add minify build step

---
+++ src/build.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const minifier = require("html-minifier").minify;
+
+const exists = path => {
+  try {
+    fs.accessSync(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const html = fs.readFileSync(`src/index.html`, "utf-8");
+const minified = minifier(html, {
+  collapseWhitespace: true,
+  decodeEntities: true,
+  minifyCSS: true,
+  removeAttributeQuotes: true,
+  sortAttributes: true,
+  sortClassName: true
+});
+
+if (!exists("docs")) {
+  fs.mkdirSync("docs");
+}
+fs.writeFileSync("docs/index.html", minified);
